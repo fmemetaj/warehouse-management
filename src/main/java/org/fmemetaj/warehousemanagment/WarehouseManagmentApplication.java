360bdffb1fe048cae00dd3c9a4bbc3af939ae9ba
Federico Memetaj <fmemetaj@gmail.com>
2024-10-13 13:59:20 +0200

    Initial commit

---
+++ src/main/java/org/fmemetaj/warehousemanagment/WarehouseManagmentApplication.java
@@ -0,0 +1,13 @@
+package org.fmemetaj.warehousemanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagmentApplication.class, args);
+	}
+
+}
